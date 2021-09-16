5e943bf1d9b4476b656aff77a408e9180b8e86f9
erik.reindl <rubes999@seznam.cz>
2021-09-16 14:57:31 +0200

    to je jedno

--- src/commands/Main.java
+++ src/commands/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //test
+        //test25252
         Scanner sc = new Scanner(System.in);
         System.out.println("Zvol si operaci");
         System.out.println("1 pro scitani");
